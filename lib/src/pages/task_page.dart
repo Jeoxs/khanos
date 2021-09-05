874f6a6556005e68f6d24aeb1417d7242414b4a4
José Aponte <jeygi9@gmail.com>
2021-09-04 21:03:17 -0400

    added validation of permission to subtasks

--- lib/src/pages/task_page.dart
+++ lib/src/pages/task_page.dart
@@ -69,7 +69,7 @@ class _TaskPageState extends State<TaskPage> {
             backgroundColor: Colors.blue,
             heroTag: "subtaskListHero",
             onPressed: () {
-              Navigator.pushNamed(context, 'subtask', arguments: {'task': task})
+              Navigator.pushNamed(context, 'subtask', arguments: {'task': task,'userRole':userRole})
                   .then((_) => setState(() {}));
             },
             child: Icon(Icons.playlist_add_check_rounded),
